ea52aafc36b82b6357e6760964de830db226f2e2
Nicolas Choquet <nicolachoquet06250@gmail.com>
2021-09-09 10:48:29 +0200

    initialisation du projet avec reconnaissence facial sur image et sur un stream vidéo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,88 +1,11 @@
-const ENABLE_IMAGE = true;
-const ENABLE_VIDEO = false;
+if (!localStorage.getItem('selected-tab')) {
+    localStorage.setItem('selected-tab', 'upload-images');
+}
 
 const imageContainer = document.querySelector('.image-container');
 const videoContainer = document.querySelector('.video-container');
 
-if (ENABLE_IMAGE) {
-    const input = document.createElement('input');
-    input.type = 'file';
-    input.name = 'uploadField';
-    input.id = 'uploadField';
-    input.classList.add('uploadField');
-    imageContainer.appendChild(input);
-
-    const uploadedImages = document.createElement('div');
-    uploadedImages.classList.add('uploadedImage');
-    imageContainer.appendChild(uploadedImages);
-
-    const uploadedFile = document.querySelector('#uploadField');
-    const uploadedImageDiv = document.querySelector('.uploadedImage');
-
-    const MODELS_LOCATION = './assets/models';
-    let modelsLoaded = [];
-
-    faceapi.nets.tinyFaceDetector.loadFromUri(MODELS_LOCATION).then(() => {
-        modelsLoaded = [...modelsLoaded, 'tinyFaceDetector loaded'];
-    });
-    faceapi.nets.ssdMobilenetv1.loadFromUri(MODELS_LOCATION).then(() => {
-        modelsLoaded = [...modelsLoaded, 'ssdMobilenetv1 loaded'];
-    });
-
-    const processImage = ({ image, imageDimensions }) => {
-        if (modelsLoaded.length !== 2) {
-            return;
-        }
-
-        faceapi.detectAllFaces(image).then(facesDetected => {
-            console.log(facesDetected);
-            const facesDetectedImage = faceapi.resizeResults(image, imageDimensions);
-            const canvas = faceapi.createCanvasFromMedia(facesDetectedImage);
-            faceapi.draw.drawDetections(canvas, facesDetected);
-            uploadedImageDiv.appendChild(canvas);
-
-            canvas.style.position = 'absolute';
-            canvas.style.top = uploadedImageDiv.firstChild.y + 'px';
-            canvas.style.left = uploadedImageDiv.firstChild.x + 'px';
-
-            if (imageDimensions.width > 1000 && imageDimensions.height > 1000) {
-                canvas.style.width = `calc(${imageDimensions.width}px / 2)`;
-            }
-
-            image.remove();
-
-            facesDetected.map(face => {
-                faceapi.draw.drawDetections(canvas, face);
-            })
-        });
-    }
-
-    const getImage = function() {
-        uploadedImageDiv.innerHTML = '';
-        const imageToProcess = this.files[0];
-        console.log('image', imageToProcess);
-
-        const image = new Image(imageToProcess.width, imageToProcess.height);
-        image.src = URL.createObjectURL(imageToProcess);
-        uploadedImageDiv.appendChild(image);
-
-        image.addEventListener('load', () => {
-            const data = {
-                image,
-                imageDimensions: {
-                    width: image.width,
-                    height: image.height
-                }
-            }
-            processImage(data);
-        })
-
-    };
-
-    uploadedFile.addEventListener('change', getImage, false);
-}
-
-if (ENABLE_VIDEO) {
+const createStream = () => {
     const videoElement = document.createElement('video');
     videoElement.id = 'inputVideo';
     videoElement.width = 720;
@@ -160,4 +83,106 @@ if (ENABLE_VIDEO) {
             faceapi.nets.faceExpressionNet.loadFromUri("./assets/models"),
         ]).then(playCam);
     })();
+};
+
+const createImageUploader = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.name = 'uploadField';
+    input.id = 'uploadField';
+    input.classList.add('uploadField');
+    imageContainer.appendChild(input);
+
+    const uploadedImages = document.createElement('div');
+    uploadedImages.classList.add('uploadedImage');
+    imageContainer.appendChild(uploadedImages);
+
+    const uploadedFile = document.querySelector('#uploadField');
+    const uploadedImageDiv = document.querySelector('.uploadedImage');
+
+    const MODELS_LOCATION = './assets/models';
+    let modelsLoaded = [];
+
+    faceapi.nets.tinyFaceDetector.loadFromUri(MODELS_LOCATION).then(() => {
+        modelsLoaded = [...modelsLoaded, 'tinyFaceDetector loaded'];
+    });
+    faceapi.nets.ssdMobilenetv1.loadFromUri(MODELS_LOCATION).then(() => {
+        modelsLoaded = [...modelsLoaded, 'ssdMobilenetv1 loaded'];
+    });
+
+    const processImage = ({ image, imageDimensions }) => {
+        if (modelsLoaded.length !== 2) {
+            return;
+        }
+
+        faceapi.detectAllFaces(image).then(facesDetected => {
+            console.log(facesDetected);
+            const facesDetectedImage = faceapi.resizeResults(image, imageDimensions);
+            const canvas = faceapi.createCanvasFromMedia(facesDetectedImage);
+            faceapi.draw.drawDetections(canvas, facesDetected);
+            uploadedImageDiv.appendChild(canvas);
+
+            canvas.style.position = 'absolute';
+            canvas.style.top = uploadedImageDiv.firstChild.y + 'px';
+            canvas.style.left = uploadedImageDiv.firstChild.x + 'px';
+
+            if (imageDimensions.width > 1000 && imageDimensions.height > 1000) {
+                canvas.style.width = `calc(${imageDimensions.width}px / 2)`;
+            }
+
+            image.remove();
+
+            facesDetected.map(face => {
+                faceapi.draw.drawDetections(canvas, face);
+            })
+        });
+    }
+
+    const getImage = function() {
+        uploadedImageDiv.innerHTML = '';
+        const imageToProcess = this.files[0];
+        console.log('image', imageToProcess);
+
+        const image = new Image(imageToProcess.width, imageToProcess.height);
+        image.src = URL.createObjectURL(imageToProcess);
+        uploadedImageDiv.appendChild(image);
+
+        image.addEventListener('load', () => {
+            const data = {
+                image,
+                imageDimensions: {
+                    width: image.width,
+                    height: image.height
+                }
+            }
+            processImage(data);
+        })
+
+    };
+
+    uploadedFile.addEventListener('change', getImage, false);
+};
+
+document.querySelector('.upload-images').addEventListener('click', () => {
+    localStorage.setItem('selected-tab', 'upload-images');
+    document.querySelector('.upload-images').parentElement.classList.add('active');
+    videoContainer.innerHTML = '';
+    createImageUploader();
+});
+
+document.querySelector('.stream').addEventListener('click', () => {
+    localStorage.setItem('selected-tab', 'stream');
+    document.querySelector('.stream').parentElement.classList.add('active');
+    imageContainer.innerHTML = '';
+    createStream();
+});
+
+if (localStorage.getItem('selected-tab') === 'upload-images') {
+    document.querySelector('.upload-images').parentElement.classList.add('active');
+    imageContainer.classList.add('active');
+    createImageUploader();
+} else {
+    document.querySelector('.stream').parentElement.classList.add('active');
+    videoContainer.classList.add('active');
+    createStream();
 }
